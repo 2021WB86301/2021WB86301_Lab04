a513350348378b43d825252b40b10107f0e9b8a4
2021WB86301 <2021wb86301@wilp.bits-pilani.ac.in>
2026-09-22 14:27:02 +0530

    Change product details from Mouse to Desktop

--- src/main/java/com/bits/App.java
+++ src/main/java/com/bits/App.java
@@ -3,10 +3,10 @@ public class App {
     public static void main(String[] args) {
         System.out.println("DevOps Lab M4 - 2021WB86301 - Java 21 - Product Scenario");
         Product p1 = new Product(101, "Laptop", 55000.0);
-        Product p2 = new Product(102, "Mouse", 500.0);
+        Product p2 = new Product(102, "Desktop", 100000.0);
         p1.display();
         p2.display();
         System.out.println("Products Created Successfully!");
-         System.out.println("Products Added  Successfully!");
+         System.out.println("Products Details Changed Successfully!");
     }
 }
